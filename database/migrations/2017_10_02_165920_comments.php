98223ba18125d0dd1525c375e33b4db8e26bb3c1
Adewale Adeleye <Adewagold@yahoo.com>
2017-10-02 18:25:51 +0100

    Migration created

--- database/migrations/2017_10_02_165920_comments.php
+++ database/migrations/2017_10_02_165920_comments.php
@@ -13,7 +13,47 @@ class Comments extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('comments', function (Blueprint $table) {
+ 
+           $table->increments('id');
+ 
+           $table->text('comment');
+ 
+           $table->integer('votes')->default(0);
+ 
+           $table->integer('spam')->default(0);
+ 
+           $table->integer('reply_id')->default(0);
+ 
+           $table->string('page_id')->default(0);
+ 
+           $table->integer('users_id');
+ 
+           $table->timestamps();
+ 
+       });
+ 
+ 
+ 
+       Schema::create('comment_user_vote', function (Blueprint $table) {
+ 
+           $table->integer('comment_id');
+ 
+           $table->integer('user_id');
+ 
+           $table->string('vote',11);
+ 
+       });
+ 
+ 
+ 
+       Schema::create('comment_spam', function (Blueprint $table) {
+ 
+           $table->integer('comment_id');
+ 
+           $table->integer('user_id');
+ 
+       });
     }
 
     /**
